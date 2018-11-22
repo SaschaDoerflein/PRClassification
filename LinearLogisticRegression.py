3afeb9bab0dc3ebef63d9041a6d9dfaf856134e0
SaschaDoerflein <sascha_doerflein@web.de>
2018-11-22 08:50:09 +0100

    Debugged version

--- LinearLogisticRegression.py
+++ LinearLogisticRegression.py
@@ -1,71 +1,70 @@
-#    Copyright 2016 Stefan Steidl
-#    Friedrich-Alexander-Universität Erlangen-Nürnberg
-#    Lehrstuhl für Informatik 5 (Mustererkennung)
-#    Martensstraße 3, 91058 Erlangen, GERMANY
-#    stefan.steidl@fau.de
-
-
-#    This file is part of the Python Classification Toolbox.
-#
-#    The Python Classification Toolbox is free software: 
-#    you can redistribute it and/or modify it under the terms of the 
-#    GNU General Public License as published by the Free Software Foundation, 
-#    either version 3 of the License, or (at your option) any later version.
-#
-#    The Python Classification Toolbox is distributed in the hope that 
-#    it will be useful, but WITHOUT ANY WARRANTY; without even the implied
-#    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
-#    See the GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with the Python Classification Toolbox.  
-#    If not, see <http://www.gnu.org/licenses/>.
-
-
 import numpy
 import numpy.matlib
 
-
 class LinearLogisticRegression(object):
 
-    # learningRate is the step length for Newton's method
-    # maxIterations is the maximum number of iterations for learning the parameters
     def __init__(self, learningRate = 0.5, maxIterations = 100):
-        self.__LRate = learningRate
+        self.__learningRate = learningRate
         self.__maxIterations = maxIterations
-        self.__t = numpy.array([1, 1])
+        self.__theta = numpy.array([1, 1, 1])
 
     def fit(self, X, y):
-        self.__X = X
-        self.__m = len(X)
+        b = numpy.ones((X.shape[0], 1))
+        self.__X = numpy.concatenate((X,b), axis=1)
         self.__Y = y
-        self.__t = self.NRSteps(self.__maxIterations, self.__t)
+        if len(set(self.__Y)) > 2:
+            return
+        self.__y1 = list(set(self.__Y))[0]
+        self.__y2 = list(set(self.__Y))[1]
+        mapToZeroOne = lambda x:  0 if x == self.__y1 else  1
+        self.__Y = numpy.array([mapToZeroOne(yi) for yi in self.__Y])
+        self.__m = len(X)
+        self.__theta = self.newtonIteration(self.__maxIterations, self.__theta)
 
-    def gFunc(self, X, t):
-        return numpy.reciprocal(1 + numpy.exp(-numpy.dot(t, X)))
+    def gFunc(self, X, theta):
+        if numpy.dot(theta, X) >= 0:
+            return 1 / (1 + numpy.exp(-numpy.dot(theta, X)))
+        else:
+            tmp = numpy.exp(numpy.dot(theta, X))
+            return tmp / (tmp + 1)
 
-    def NRSteps(self, iterations, t):
-        for i in range(0, iterations):
-            t -= numpy.reciprocal(self.hessian(t + self.__LRate) * self.gradient(t + self.__LRate))
-        return t
+    def gradient(self, theta):
+        summe = 0.0
+        for i in range(0,self.__m): summe += (self.__Y[i] - self.gFunc(self.__X[i], theta)) * self.__X[i]
+        return summe
 
-    def gradient(self, t):
-        sum = 0
-        for i in range(0, self.__m): sum += (self.__Y[i] - self.gFunc(numpy.dot(t, self.__X[i]))) * self.__X[i]
-        return sum
-
-    def hessian(self, t):
-        summe = 0
+    def hessianLikeliHood(self, theta):
+        summe = 0.0
         for i in range(0, self.__m):
-            g = self.gFunc(t, self.__X[i])
+            g = self.gFunc(theta, self.__X[i])
             summe += g * (1 - g) * numpy.dot(self.__X[i], self.__X[i])
         return -summe
 
-    def predict(self, X):
-        if self.gFunc(X, self.__t) >= 0.5:
-            return 1
-        else:
-            return 0
+    def lFunc(self, theta):
+        summe = 0.0
+        for i in range(0, self.__m):
+            summe += self.__Y[i] * numpy.dot(theta, self.__X[i]) + numpy.log(1 - self.gFunc(self.__X[i], theta))
+        return summe
+    def newtonIteration(self, iterations, startTheta):
+        theta = startTheta
+        for i in range(0, iterations):
+            print (self.lFunc(theta))
+            if numpy.sum(numpy.absolute((self.gradient(theta)))) < 1e-20:
+                return theta
+            if self.hessianLikeliHood(theta) == 0.0:
+                return theta
+            theta = theta -  self.__learningRate * 1/self.hessianLikeliHood(theta) * self.gradient(theta)
+        return theta
 
+    def predict(self, X):
+        b = numpy.ones((X.shape[0],1))
+        X = numpy.concatenate((X,b), axis=1)
+        indices = numpy.zeros(len(X))
+        for i in range (0, len(X)):
+            if self.gFunc(X[i], self.__theta) > 0.5:
+                indices[i] = self.__y2
+            else:
+                indices[i] = self.__y1
+        return indices
 
 
